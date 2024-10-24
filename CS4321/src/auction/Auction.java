064f748963ec871d731da58f856f88a3aeb09d9d
Finch McCambridge <89469573+Zedsdemise@users.noreply.github.com>
2024-10-24 11:30:38 -0400

    Delete CS4321 directory
    Needed to restructure the folder and change the name to a more descriptive title

--- CS4321/src/auction/Auction.java
+++
@@ -1,8 +0,0 @@
-package auction;
-public class Auction {
-    public String placeBid (user user, item item, double bid) {
-        if (bid > item.getCurrentBid()){
-            item.addBid(item.getID(), bid);
-        }
-    }
-}
