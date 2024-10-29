5793a5226b34415fc0d4ffa88b15b301e3ceea4f
Miguel Uriostigue <muriostigue@valdosta.edu>
2024-10-29 12:00:02 -0400

    Added gui elements for user story 2 and JUnit for category class.

--- AuctionGui/src/application/Main.java
+++ AuctionGui/src/application/Main.java
@@ -5,24 +5,29 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	private ObservableList<String> categories;
 	private CategoryController categoryController;
+	private CommissionController commissionController;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 //			BorderPane root = new BorderPane();
 			CategoryManager categoryManager = new CategoryManager();
 			categoryController = new CategoryController(categoryManager);
+			commissionController = new CommissionController();
 			// US-1
 			TextField categoryField = new TextField("Enter category name");
 	        Button addButton = new Button("Add Category");
@@ -39,10 +44,46 @@ public class Main extends Application {
 				
 			});		
 	        
-			VBox vBox = new VBox(categoryField, addButton, categoryListView);
-			vBox.setSpacing(10);
+			
+			// US-2
+			TextField commissionField = new TextField("Enter commission");
+			Button setCommissionButton = new Button("Set Seller Commission");
+			Label currentCommissionLbl = new Label("Current Commission: 0%");
+			
+			setCommissionButton.setOnAction(e -> {
+                String commissionText = commissionField.getText().trim();
+                try {
+                    double commissionValue = Double.parseDouble(commissionText);
+                    commissionController.setSellerCommission(commissionValue);
+                    currentCommissionLbl.setText("Current Commission: " + commissionController.getSellerCommission() + "%");
+                    commissionField.clear();
+                } 
+                catch (NumberFormatException ex) {
+                    showAlert("Invalid Input", "Please enter a valid number for commission percentage.");
+                } 
+                catch (IllegalArgumentException ex) {
+                    showAlert("Invalid Input", ex.getMessage());
+                }
+             });
+			
+			// US-3
+			TextField premiumField = new TextField("Enter premium");
+			Button setPremiumButton = new Button("Set Buyer Premium ");
+			Label currentPremiumLbl = new Label("Current Premium: 0%");
+			
+			VBox categoryBox = new VBox(categoryField, addButton, categoryListView);
+			categoryBox.setSpacing(10);
+			
+			VBox commissionBox = new VBox(commissionField, setCommissionButton, currentCommissionLbl, premiumField, setPremiumButton, currentPremiumLbl);
+			commissionBox.setSpacing(10);
+			
+			HBox hBox = new HBox(categoryBox, commissionBox);
+			hBox.setSpacing(20);
+			
+//			VBox vBox = new VBox(categoryField, addButton, categoryListView, commissionField, setCommissionButton, currentCommissionLbl, premiumField, setPremiumButton, currentPremiumLbl);
+//			vBox.setSpacing(10);
 	        
-			Scene scene = new Scene(vBox,400,400);
+			Scene scene = new Scene(hBox,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -55,6 +96,14 @@ public class Main extends Application {
         categories.clear();
         List<String> categoryNames = categoryController.getCategories();
         categories.addAll(categoryNames);
+    }
+	
+	private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 	
 	
