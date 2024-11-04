d4e21f1199e83da473fdad399a1a0ffa8d9f549f
Finch McCambridge <finchmccambridge01@gmail.com>
2024-11-04 15:37:08 -0500

    Created User
    As rudimentary as possible, shouldn't need to improve it until sprint 2

---
+++ auction_system/src/auction_system/User.java
@@ -0,0 +1,13 @@
+package auction_system;
+
+public class User{
+    private int id;
+
+    public User(int id){
+        this.id = id;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+}
