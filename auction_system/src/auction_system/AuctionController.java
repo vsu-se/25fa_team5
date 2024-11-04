84c0310e37d86d767508e870482f2c4e59cd17f4
Kathleen Mock <kmock@valdosta.edu>
2024-11-04 10:29:44 -0500

    added some classes for auctions, save file, data persistence class.

---
+++ auction_system/src/auction_system/AuctionController.java
@@ -0,0 +1,12 @@
+package auction_system;
+
+public class AuctionController {
+//	private AuctionGui gui;
+//	private AuctionManager auctionManager;
+//	
+//	public AuctionController(AuctionGui gui) {
+//		this.gui = gui;
+//		this.auctionManager = auctionManager;
+//	}
+		 
+}
