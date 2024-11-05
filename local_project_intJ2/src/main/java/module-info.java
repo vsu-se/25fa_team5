68015cc23f61ba5506505591143673fbfeae590d
Miguel Uriostigue <muriostigue@valdosta.edu>
2024-11-05 11:21:41 -0500

    Delete local_project_intJ2 directory

--- local_project_intJ2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.local_project_intj2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.local_project_intj2 to javafx.fxml;
-    exports com.example.local_project_intj2;
-}
