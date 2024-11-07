62fe846ab1c6db88f774277f3e086aa747e28409
Kathleen Mock <kmock@valdosta.edu>
2024-11-07 13:43:35 -0500

    Delete AuctionGui/src/auction_system directory

--- AuctionGui/src/auction_system/Auction.java
+++
@@ -1,8 +0,0 @@
-package auction_system;
-public class Auction {
-//    public String placeBid (User user, Item item, double bid) {
-//        if (bid > item.getCurrentBid()){
-//            item.addBid(user.getID(), bid);
-//        }
-    }
-
