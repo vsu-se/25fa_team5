1d7f68977019a114af0ffcc22e421a207bcdac22
Finch McCambridge <finchmccambridge01@gmail.com>
2024-11-12 12:33:27 -0500

    Add name variable to User
    User now has a String variable for usernames. Along with the expected getter and setter, there are two methods that, if given a name or ID, will return the other. Hopefully, this will allow future methods in other classes to not always require an entire User as an input for proper function

--- auction_system/src/auction_system/User.java
+++ auction_system/src/auction_system/User.java
@@ -2,8 +2,9 @@ package auction_system;
 
 public class User{
     private int id;
+    private String name;
 
-    public User(int id){
+    public User(int id, String name){
         this.id = id;
     }
 
@@ -13,5 +14,25 @@ public class User{
     
     public void setID(int id) {
     	this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIDGivenName(String userName) {
+        if(userName.equals(getName())){
+            return getID();
+        }
+    }
+
+    public String getNameGivenID(int id) {
+        if(id == getID()) {
+            return getName();
+        }
     }
 }
