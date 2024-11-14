9e7c71cb4c1e3bf806fed001cbb592b78c65016f
Finch McCambridge <finchmccambridge01@gmail.com>
2024-11-14 12:24:48 -0500

    Fix Constructor, toString
    The item constructor and toString have been fixed to represent the better connection between Item and User.

--- auction_system/src/auction_system/Item.java
+++ auction_system/src/auction_system/Item.java
@@ -1,39 +1,42 @@
 package auction_system;
 //import java.util.*;
+
 //import java.lang.Double;
 
 public class Item {
-    private int id;
-    private String name;
-    private User user;
-      
-    public Item (int id, String name, User user){
-        this.id = id;
-        this.name = name;
-        this.user = user;
-    }
+	private int id;
+	private String name;
+	private User user;
 
-    public int getID () {
-    	return this.id;
-    }
-    
-    public String getName() {
-    	return this.name;
-    }
-    
-    public void setName (String name) {
-    	this.name = name;
-    }
-    
-    public void setID (int ID) {
-    	this.id = ID;
-    }
+	public Item(int id, String name, User user) {
+		this.id = id;
+		this.name = name;
+		this.user = user;
+	}
 
-    @Override
-    public String toString () {
-    	String line1 = "Item ID: " +  id + "\n";
-    	String line2 = "Item Name: "+ name + "\n";
-    	return line1+line2;
-    }
+	public int getID() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setID(int ID) {
+		this.id = ID;
+	}
+
+	@Override
+	public String toString() {
+		String line1 = "Item ID: " + id + "\n";
+		String line2 = "Item Name: " + name + "\n";
+		String line3 = "Seller ID: " + user.getID() + "\n";
+		String line4 = "Seller Name: " + user.getName() + "\n";
+		return line1 + line2 + line3 + line4;
+	}
 
 }
