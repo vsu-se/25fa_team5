5b7c2d3e18da12ae03ba558ba5beaa89e5d2c551
Finch McCambridge <finchmccambridge01@gmail.com>
2024-11-14 12:27:04 -0500

    Change User
    Two methods in user; getIDGivenName and getNameGivenID, have been commented out and may be entirely removed at a later date. They were expected to be used when I changed Auction.bids while getting the date/time to record properly

--- auction_system/src/auction_system/User.java
+++ auction_system/src/auction_system/User.java
@@ -1,38 +1,42 @@
 package auction_system;
 
-public class User{
-    private int id;
-    private String name;
+public class User {
+	private int id;
+	private String name;
 
-    public User(int id, String name){
-        this.id = id;
-    }
+	public User(int id, String name) {
+		this.id = id;
+	}
 
-    public int getID(){
-        return this.id;
-    }
-    
-    public void setID(int id) {
-    	this.id = id;
-    }
+	public int getID() {
+		return this.id;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public void setID(int id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public int getIDGivenName(String userName) {
-        if(userName.equals(getName())){
-            return getID();
-        }
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getNameGivenID(int id) {
-        if(id == getID()) {
-            return getName();
-        }
-    }
+//    public int getIDGivenName(String userName) {
+//    	int id = 0;
+//        if(userName.equals(getName())){
+//            id = this.id;
+//        }
+//		return id;
+//    }
+//
+//    public String getNameGivenID(int id) {
+//    	String name = "Null";
+//        if(id == getID()) {
+//            name = this.name;
+//        }
+//        return name;
+//    }
 }
