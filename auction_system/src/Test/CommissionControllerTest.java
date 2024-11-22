f54b830b57b50eb2d68250f5083fe7827aea2dc2
Miguel Uriostigue <muriostigue@valdosta.edu>
2024-11-21 19:02:31 -0500

    Update junit test cases.

--- auction_system/src/Test/CommissionControllerTest.java
+++ auction_system/src/Test/CommissionControllerTest.java
@@ -15,30 +15,30 @@ class CommissionControllerTest {
     }
 
     @Test
-    public void testInitialBuyerPremiumIsZero() {
+    public void testInitialSellerCommissionIsZero() {
         assertEquals(0.0, controller.getSellerCommission(), "Initial commission should be 0.0");
     }
 
     @Test
-    public void testSetValidBuyerPremium() {
+    public void testSetValidSellerCommission() {
         controller.setSellerCommission("25.5");
         assertEquals(25.5, controller.getSellerCommission(), "Commission should be set to 25.5");
     }
 
     @Test
-    public void testSetBuyerPremiumAtLowerBoundary() {
+    public void testSetSellerCommissionAtLowerBoundary() {
         controller.setSellerCommission("0.0");
         assertEquals(0.0, controller.getSellerCommission(), "Commission should be set to 0.0");
     }
 
     @Test
-    public void testSetBuyerPremiumAtUpperBoundary() {
+    public void testSetSellerCommissionAtUpperBoundary() {
         controller.setSellerCommission("100.0");
         assertEquals(100.0, controller.getSellerCommission(), "Commission should be set to 100.0");
     }
 
     @Test
-    public void testSetBuyerPremiumBelowLowerBoundary() {
+    public void testSetSellerCommissionBelowLowerBoundary() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             controller.setSellerCommission("-1.0");
         });
@@ -46,7 +46,7 @@ class CommissionControllerTest {
     }
 
     @Test
-    public void testSetBuyerPremiumAboveUpperBoundary() {
+    public void testSetSellerCommissionAboveUpperBoundary() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             controller.setSellerCommission("101.0");
         });
@@ -54,7 +54,7 @@ class CommissionControllerTest {
     }
 
     @Test
-    public void testSetBuyerPremiumWithInvalidNumberFormat() {
+    public void testSetSellerCommissionWithInvalidNumberFormat() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             controller.setSellerCommission("invalid");
         });
@@ -62,7 +62,7 @@ class CommissionControllerTest {
     }
 
     @Test
-    public void testSetBuyerPremiumWithEmptyString() {
+    public void testSetSellerCommissionWithEmptyString() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             controller.setSellerCommission("");
         });
