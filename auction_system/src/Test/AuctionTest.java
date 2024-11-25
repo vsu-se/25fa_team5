0a682ec11a9c6473f802f0781b5302bad8fc928d
Finch McCambridge <finchmccambridge01@gmail.com>
2024-11-25 12:14:53 -0500

    Fix AuctionTests
    AuctionTest has been fixed to reflect the current method of item and user construction and control. Current tests resolve properly

--- auction_system/src/Test/AuctionTest.java
+++ auction_system/src/Test/AuctionTest.java
@@ -1,42 +1,57 @@
 package Test;
 
 import org.junit.jupiter.api.*;
+
+import auction_system.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Date;
 
 class AuctionTest {
 
-    private Auction auction;
-    private User user;
-    private Item item;
+    Auction auction;
+    User user;
+    Item item;
 
     @BeforeEach
     void setUp() {
-        user = new User(12345, "Finch");
-        item = new Item(11111, "Cool Hat", user);
+
+        ItemManager itemManager = new ItemManager();
+        UserManager userManager = new UserManager();
+        AuctionManager auctionManager = new AuctionManager();
+        ItemController itemController = new ItemController(itemManager);
+        UserController userController = new UserController(userManager);
+        user = userController.createUser("Finch");
+        item = itemController.createItem("Cool Hat", user);
         auction = new Auction(item, 50.0, 100.0);
+        auctionManager.addAuction(auction);
+        
     }
 
     @Test
     void testAddBidAccepted() {
+    	System.out.println("Testing AddBid for accept condition");
         auction.addBid(user, 60.0);
         assertEquals(60.0, auction.getCurrentBid());
     }
 
     @Test
     void testAddBidRejected() {
+    	System.out.println("Testing AddBid for reject condition");
         auction.addBid(user, 40.0);
         assertNotEquals(40.0, auction.getCurrentBid());
     }
 
     @Test
     void testEndAuction() {
+    	System.out.println("Testing endAuction");
         auction.endAuction();
         assertFalse(auction.getActive());
     }
 
     @Test
     void testCheckDateAuctionEnded() {
+    	System.out.println("Testing checkDate on an auction that has ended");
         auction.setEndDate(new Date(System.currentTimeMillis() - 1000)); // Set end date in the past
         auction.checkDate();
         assertFalse(auction.getActive());
@@ -44,6 +59,7 @@ class AuctionTest {
 
     @Test
     void testCheckDateAuctionOngoing() {
+    	System.out.println("Testing checkDate on an auction that is ongoing");
         auction.checkDate();
         assertTrue(auction.getActive());
     }
