741c05fa726a2b6baa9de01a3c3521a91faa4269
Christian McCambridge <finchmccambridge01@gmail.com>
2024-12-03 14:25:07 -0500

    Change User User has been simplified to only contain a username, as a duplication prevention system already exists and invalidates the need for a unique userID.

--- auction_system/src/auction_system/User.java
+++ auction_system/src/auction_system/User.java
@@ -1,21 +1,10 @@
 package auction_system;
 
 public class User {
-	private int id;
 	private String name;
 
-	public User(int id, String name) { // Constructor needs to be reworked to auto generate id to a specified format that checks for duplicates
-		this.id = id;
+	public User(String name) {
 		this.name = name;
-		// After the user is constructed, it will need to be added to the userList in UserManager
-	}
-
-	public int getID() {
-		return this.id;
-	}
-
-	public void setID(int id) {
-		this.id = id;
 	}
 
 	public String getName() {
