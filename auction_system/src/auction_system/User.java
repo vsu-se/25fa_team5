6a276b89cea728c15072713d500af528cde72ccb
Christian McCambridge <finchmccambridge01@gmail.com>
2024-12-02 19:05:08 -0500

    Fix User.java I must have uploaded an older version of User at some point, but the constructor now reflects the way that ID generation is taken by using createUser in UserController. I should, in theory, fix the JUnit tests for User because of this. But we're behind schedule as is.

--- auction_system/src/auction_system/User.java
+++ auction_system/src/auction_system/User.java
@@ -4,8 +4,8 @@ public class User {
 	private int id;
 	private String name;
 
-	public User(String name) { // Constructor needs to be reworked to auto generate id to a specified format that checks for duplicates
-		this.id = generateUserID();
+	public User(int id, String name) { // Constructor needs to be reworked to auto generate id to a specified format that checks for duplicates
+		this.id = id;
 		this.name = name;
 		// After the user is constructed, it will need to be added to the userList in UserManager
 	}
