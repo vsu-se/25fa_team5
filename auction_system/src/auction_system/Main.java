7d396762f5af32b37dacbe52ef875e22002fdc7d
Miguel Uriostigue <muriostigue@valdosta.edu>
2024-12-03 12:12:09 -0500

    Added tab pane to registered user.

--- auction_system/src/auction_system/Main.java
+++ auction_system/src/auction_system/Main.java
@@ -313,7 +313,6 @@ public class Main extends Application {
 					showAlert("Error", "All fields must be filled.");
 				}
 			});
-
 			// Reports tab
 			Tab reportsTab = new Tab("Reports");
 			reportsTab.setClosable(false);
