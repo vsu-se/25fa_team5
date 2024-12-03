19eb71d8b060b62f673d2b8ec35d6dee0ec9b29a
Christian McCambridge <finchmccambridge01@gmail.com>
2024-12-02 19:02:09 -0500

    Change AuctionManager.java In an attempt to solve the user story related to time keeping, I have included a ScheduledExecutorService in AuctionManager to be used in a ScheduledThreadPool that calls the new checkAuctionStatus every minute.

--- auction_system/src/auction_system/AuctionManager.java
+++ auction_system/src/auction_system/AuctionManager.java
@@ -1,14 +1,21 @@
 package auction_system;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class AuctionManager {
     private ArrayList<Auction> auctionList = new ArrayList<>();
     private ArrayList<Auction> inactiveAuctionList = new ArrayList<>();
     private ArrayList<Auction> activeAuctionList = new ArrayList<>();
+    private ScheduledExecutorService scheduler;
 
     public AuctionManager() {
-
+    scheduler = Executors.newScheduledThreadPool(1);
+    scheduler.scheduleAtFixedRate(this::checkAuctionStatus, 0, 1, TimeUnit.MINUTES);
     }
 
     public void addAuction(Auction auction) {
@@ -23,6 +30,25 @@ public class AuctionManager {
     	activeAuctionList.remove(auction);
     	inactiveAuctionList.add(auction);
     	auction.endAuction();
+    }
+
+    private void checkAuctionStatus() {
+        Date now = new Date();
+        List<Auction> endedAuctions = new ArrayList<>();
+        for(Auction auction : activeAuctionList){
+            if(auction.getEndDate().before(now)){
+                endedAuctions.add(auction);
+            }
+        }
+        for(Auction auction : endedAuctions) {
+            endAuction(auction);
+        }
+    }
+
+    public void shutDownScheduler() {
+        if(scheduler != null && !scheduler.isShutdown()){
+            scheduler.shutdown();
+        }
     }
 
     public boolean containsAuction(Auction auction) {
