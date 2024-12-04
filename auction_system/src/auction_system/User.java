71a006a15fa71fb1956b373453e9d94c080ef1a5
Finch McCambridge <finchmccambridge01@gmail.com>
2024-12-04 13:54:54 -0500

    Change User
    To ensure the project as a whole can compile, the old version of the User constructor was added without the generateID function being used.

--- auction_system/src/auction_system/User.java
+++ auction_system/src/auction_system/User.java
@@ -2,8 +2,10 @@ package auction_system;
 
 public class User {
 	private String name;
+	private int id;
 
-	public User(String name) {
+	public User(int id, String name) {
+		this.id = id;
 		this.name = name;
 	}
 
@@ -13,5 +15,18 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public void setID() {
+		this.id = id;
+	}
+
+	@Override
+	public String toString(){
+		String line1 = "Username: " + name;
 	}
 }
