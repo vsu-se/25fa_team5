25caa6cf63d2d984c9f8d75d1888cffc9d5a62ee
Finch McCambridge <finchmccambridge01@gmail.com>
2024-12-04 12:36:28 -0500

    Remove UserTest, ItemTest
    The current versions of both Item and User do not contain any logic and, therefore, do not require JUnit tests. Later these files will be removed entirely, but for now are emptied

--- auction_system/src/Test/UserTest.java
+++ auction_system/src/Test/UserTest.java
@@ -8,28 +8,5 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
-	
-	User user;
-	
-	@BeforeEach
-	public void init() {
-		user = new User(12345);
-	}
-	
-	@AfterEach
-	public void reset() {
-		user.setID(12345);
-	}
-
-	@Test
-	void testGetID() {
-		assertEquals(12345, user.getID(), "getID() does not return the expected value");
-	}
-	
-	@Test
-	void testSetID() {
-		user.setID(12346);
-		assertEquals(12346, user.getID(), "setID() does not set ID to the expected value");
-	}
 
 }
