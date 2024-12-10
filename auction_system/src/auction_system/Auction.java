da2725cde43286410ad4898470a2eb4d8dcf6ce8
Kathleen Mock <kmock@valdosta.edu>
2024-12-09 19:17:23 -0500

    Delete unused auction methods/variables, text file, and class

--- auction_system/src/auction_system/Auction.java
+++ auction_system/src/auction_system/Auction.java
@@ -9,11 +9,8 @@ import java.util.*;
 import java.lang.Double;
 
 public class Auction {
-	TreeMap<Double, Integer> bids = new TreeMap<>();
-	TreeMap<Double, Date> bidDates = new TreeMap<>();
 	private double bIN = 0.0;
-	private Date startDate;
-	private Date endDate;
+	private Date endDate; // will delete later
 	private Item item;
 	private boolean isActive = false;
 	private boolean isBought; // update variable after auction ends and at least one bid
@@ -37,33 +34,6 @@ public class Auction {
 		this.bIN = bIN;
 		this.isActive = true;
     }
-	
-	
-	public Auction(Item item, double startBid) {
-		Calendar c = Calendar.getInstance();
-		startDate = c.getTime();
-		c.add(Calendar.DATE, 30); // Auction time set for 30 days
-		endDate = c.getTime();
-		bids.put(startBid, item.getID());
-		this.item = item;
-		this.isActive = true;
-		System.out.println("Item #" + item.getID() + " for " + item.getName() + " begins at " + startDate
-				+ " with a starting bid of $" + startBid);
-	}
-
-	public Auction(Item item, double startBid, double bIN) {
-		Calendar c = Calendar.getInstance();
-		startDate = c.getTime();
-		c.add(Calendar.DATE, 30); // Auction time set for 30 days
-		endDate = c.getTime();
-		bids.put(startBid, item.getID());
-		this.item = item;
-		this.isActive = true;
-		this.bIN = bIN;
-		System.out.println("Item #" + item.getID() + " for " + item.getName() + " begins at " + startDate
-				+ " with a starting bid of $" + startBid + " and a Buy-it-Now price of $" + bIN);
-
-	}
 
 	public void setBidManager(BidManager bidManager) {
 		this.bidManager = bidManager;
@@ -73,48 +43,12 @@ public class Auction {
 		return this.item;
 	}
 
-	public Date getStartDate() {
-		return this.startDate;
-	}
-
-	public Date getEndDate() {
-		return this.endDate;
-	}
-
 	public double getbIN() {
 		return this.bIN;
 	}
 
-	public void setEndDate(Date date) {
-		this.endDate = date;
-	}
-
 	public void setbIN(double bIN) {
 		this.bIN = bIN;
-	}
-
-	public double getCurrentBid() {
-		double currBid = 0.0;
-		if (bids.isEmpty()) {
-			return 0.0;
-		}
-		if (isActive) {
-			currBid = bids.lastKey(); // Tree maps order themselves based on the numerical value of the key, so if the
-									  // bid amount is the key instead of userID, it self sorts
-		}
-		return currBid;
-	}
-	
-	public void getAllBids() {
-		Set<Map.Entry<Double, Integer> > entrySet = bids.entrySet();
-		for(Map.Entry<Double, Integer> currentBid : entrySet) {
-			// Printing the Value (User ID)
-			System.out.print("User: " + currentBid.getValue());
-			// Printing the Key (Bid amount)
-			System.out.println(" Bid: " + currentBid.getKey());
-			// Printing the Value of bidDates (Time of Bid)
-			System.out.println("Time of Bid Placement: " + bidDates.get(currentBid.getKey()));
-		}
 	}
 
 	public void endAuction() {
@@ -152,15 +86,6 @@ public class Auction {
 	public LocalDateTime getLocalEndDateAndTime() {
 		return LocalDateTime.of(endingDate, endTime);
 	}
-	
-//	public void addBid(int userID, double bid) {
-//		if(this.getCurrentBid() < bid) {
-//			bids.put(bid, userID);
-//			System.out.println("Bid Accepted");
-//		} else {
-//			System.out.println("Bid Denied from " + user.getName() + " Lower than Current Bid");
-//		}
-//	}
 
 	public boolean addBid2(Bid bid) {
 		if(bidManager.containsBid(bid)) {
@@ -185,22 +110,6 @@ public class Auction {
 			return false;
 		}
 	}
-	
-//	@Override
-//	public String toString() {
-//		String line1 = this.item.toString();
-//		String line2 = "\nCurrent and Previous bids: " + bids.keySet();
-//		String line3 = "\nBuy-it-Now Price: " + this.getbIN();
-//		String line4 = "\nStarting Date: " + startDate;
-//		String line5 = "\nEnding Date: " + endDate;
-//		String line6;
-//		if(isActive) {
-//			line6 = "\nStatus: Active";
-//		} else {
-//			line6 = "\nStatus: Inactive";
-//		}
-//		return line1 + line2 + line3 + line4 + line5 + line6;
-//	}
 
 	public boolean findWinningBid() {
 		if(!bidManager.checkIfEmpty()) {
